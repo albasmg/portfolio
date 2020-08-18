9f1da22ca0c56870b56d96487069f431a70e5c8f
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-08-18 19:43:23 +0200

    :bug: fix docs folder

---
+++ docs/precache-manifest.22e11beb23289062edde2da134a10f9d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5dfad9b21946fe044b81078b27254b4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "418ee4021dcead346d88",
+    "url": "./static/css/main.5830950c.chunk.css"
+  },
+  {
+    "revision": "74ccdd8b7e83b3bb9d42",
+    "url": "./static/js/2.acac7a67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.acac7a67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "418ee4021dcead346d88",
+    "url": "./static/js/main.705014fd.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "dcf61cd651a952b07a4b0396fb307511",
+    "url": "./static/media/CV-AlbaSanMartín.dcf61cd6.pdf"
+  },
+  {
+    "revision": "e1d77fbeebe80b32ac9f9796b66c7951",
+    "url": "./static/media/futuramas.e1d77fbe.jpg"
+  },
+  {
+    "revision": "252def53a05a74dab89dea6586a5c7c4",
+    "url": "./static/media/profilephoto.252def53.png"
+  },
+  {
+    "revision": "7794804fbcacb1e2cc2e001b228eadf1",
+    "url": "./static/media/rickandmorty.7794804f.jpg"
+  },
+  {
+    "revision": "e73e452641b9626446d1c835d633f99b",
+    "url": "./static/media/serflix.e73e4526.jpg"
+  },
+  {
+    "revision": "0963dd34b5e44d8a9319702ed94b7e10",
+    "url": "./static/media/theateam.0963dd34.jpg"
+  }
+]);
